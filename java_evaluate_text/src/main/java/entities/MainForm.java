985c0eec9bd95b1fecff2ca846578c38b58f2346
Styvensoft <steveen.echeverri@gmail.com>
2022-12-15 22:10:25 -0500

    Created entities MainForm Basic

---
+++ java_evaluate_text/src/main/java/entities/MainForm.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entities;
+
+/**
+ *
+ * @author s7eveen
+ */
+public class MainForm {
+    
+    String name;
+    String lastname;
+    int age;
+    
+    public MainForm() {
+        
+    }
+
+    public MainForm(String name, String lastname, int age) {
+        this.name = name;
+        this.lastname = lastname;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    
+    
+}
